5c02798a87560cc7578fa4e2ad45e698449289b5
Tina Wang <fabzidentiste@gmail.com>
2022-05-03 11:51:51 +0200

    🔧 Set to bundle into one file only

--- vite.config.js
+++ vite.config.js
@@ -19,11 +19,24 @@ export default defineConfig({
   },
   
   base: '',
-  server: {
-    open: true,
-  },
   
+  server: {
+    host: '0.0.0.0',
+    open: false,
+  },
+
   build: {
-  	outDir: 'dist'
+    outDir: 'dist',
+    target: "esnext",
+		assetsInlineLimit: 100000000,
+		chunkSizeWarningLimit: 100000000,
+		cssCodeSplit: false,
+		brotliSize: false,
+		rollupOptions: {
+			inlineDynamicImports: true,
+			output: {
+				manualChunks: () => "everything.js",
+			},
+		},
   }
 });
